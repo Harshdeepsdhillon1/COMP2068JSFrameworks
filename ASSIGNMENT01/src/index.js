80ead613b16f1a2a529515ee96ce9ecdc85b8410
Harshdeepsdhillon1 <harshdeepsdhillon1@gmail.com>
2024-10-15 23:23:35 -0400

    Added projects and updated colors with more refinements such as layout

--- ASSIGNMENT01/src/index.js
+++ ASSIGNMENT01/src/index.js
@@ -4,25 +4,40 @@ const hbs = require("hbs");
 const path = require('path');
 const port = process.env.PORT || 3000; 
 
-// Set the path for views
-const viewsPath = path.join(__dirname, '../views');
-const partials_path = path.join(__dirname, '../views/partials');
+// Set the path for views and partials
+const viewsPath = path.join(__dirname, '../views'); 
+const partialsPath = path.join(__dirname, '../views/partials'); 
 
-
+// Set view engine and views directory
 app.set('view engine', 'hbs');
-app.set('views', viewsPath); // Set the directory for view templates
-app.use('/public', express.static(path.join(__dirname, "../public")))
+app.set('views', viewsPath);
+
+// Serve static files like CSS, JS, and images
+app.use('/public', express.static(path.join(__dirname, "../public")));
 app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
 app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
-hbs.registerPartials(partials_path);
 
-
-
-app.get("/", (req,res)=>{
-    res.render("home.hbs");
+// Register partials, if you use any
+hbs.registerPartials(partialsPath);
+app.use((req, res, next) => {
+    if (req.url.endsWith(".hbs")) {
+        const newUrl = req.url.replace(".hbs", "");
+        return res.redirect(newUrl);
+    }
+    next();
 });
 
-app.listen(port, ()=>{
-    console.log(`Listening to the port ${port}`);
+// Route for home page
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
-})
+// Route for projects page
+app.get("/projects", (req, res) => {
+    res.render("projects");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
